4a8cbb204e4569cf671c8c777eb9029ba052cf2a
Truong Thuan <ryantruong@MacBook-Air-cua-Truong.local>
2023-07-03 03:42:32 +0700

    useMemo()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import { useState } from "react";
+import { useState, useCallback, useRef, useMemo } from "react";
 import Content from "./Content";
 
 function App() {
-    const [show, setShow] = useState(false);
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [products, setProducts] = useState([]);
+
+    const handleClick = () => {
+        setProducts([
+            ...products,
+            {
+                name,
+                price: +price,
+            },
+        ]);
+        setName("");
+        setPrice("");
+    };
+
+    const total = useMemo(() => {
+        const result = products.reduce(
+            (result, product) => result + product.price,
+            0
+        );
+        return result;
+    }, [products]);
 
     return (
         <div className="App" style={{ padding: 50 }}>
-            <button onClick={() => setShow(!show)}>Toggle</button>
-            {show && <Content />}
+            <input
+                value={name}
+                placeholder="Enter Name"
+                onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+            <input
+                value={price}
+                placeholder="Enter Price"
+                onChange={(e) => setPrice(e.target.value)}
+            />
+            <br />
+            <button onClick={handleClick}>Add</button>
+            Total: {total}
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>
+                        {product.name}-{product.price}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,51 +1,13 @@
-import { useEffect, useState } from "react";
+import { memo } from "react";
 
-const lessons = [
-    {
-        id: 1,
-        name: "Javascript",
-    },
-    {
-        id: 2,
-        name: "HTML-CSS",
-    },
-    {
-        id: 3,
-        name: "Reacjs",
-    },
-];
-
-function Content() {
-    const [lessonId, setLessonId] = useState(1);
-
-    useEffect(() => {
-        const handleEvent = ({ detail }) => {
-            console.log(detail);
-        };
-
-        window.addEventListener(`lesson-${lessonId}`, handleEvent);
-
-        return () => {
-            window.removeEventListener(`lesson-${lessonId}`, handleEvent);
-        };
-    }, [lessonId]);
-
+function Content({ onIncrease }) {
+    console.log("re-render");
     return (
-        <div>
-            {lessons.map((lesson) => (
-                <li
-                    key={lesson.id}
-                    style={{
-                        margin: "20px",
-                        color: lessonId === lesson.id ? "orange" : "#333",
-                    }}
-                    onClick={() => setLessonId(lesson.id)}
-                >
-                    {lesson.name}
-                </li>
-            ))}
-        </div>
+        <>
+            <h2>Hello ae </h2>
+            <button onClick={onIncrease}>Click me!</button>
+        </>
     );
 }
 
-export default Content;
+export default memo(Content);
