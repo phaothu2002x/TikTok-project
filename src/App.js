d8f5315bd91d1b959cbd1edd243a1b3a73a89521
Truong Thuan <ryantruong@MacBook-Air-cua-Truong.local>
2023-07-05 03:08:12 +0700

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,51 +1,98 @@
-import { useState, useCallback, useRef, useMemo } from "react";
+import { useReducer, useRef } from "react";
 import Content from "./Content";
 
-function App() {
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
-    const [products, setProducts] = useState([]);
+//init value
+const initState = {
+    job: "",
+    jobs: [],
+};
+//actions
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
 
-    const handleClick = () => {
-        setProducts([
-            ...products,
-            {
-                name,
-                price: +price,
-            },
-        ]);
-        setName("");
-        setPrice("");
+const setJob = (payload) => {
+    return {
+        type: SET_JOB,
+        payload,
     };
+};
 
-    const total = useMemo(() => {
-        const result = products.reduce(
-            (result, product) => result + product.price,
-            0
-        );
-        return result;
-    }, [products]);
+const addJob = (payload) => {
+    return {
+        type: ADD_JOB,
+        payload,
+    };
+};
+
+const deleteJob = (payload) => {
+    return {
+        type: DELETE_JOB,
+        payload,
+    };
+};
+
+//reducer
+const reducer = (state, action) => {
+    switch (action.type) {
+        case SET_JOB:
+            return {
+                ...state, //bảo lưu cái state cạnh bên
+                job: action.payload, // thay đổi cái cần
+            };
+        case ADD_JOB:
+            return {
+                ...state,
+                jobs: [...state.jobs, action.payload],
+            };
+        case DELETE_JOB:
+            const newJobs = [...state.jobs];
+            newJobs.splice(action.payload, 1);
+            return {
+                ...state,
+                jobs: newJobs,
+            };
+        default:
+            throw new Error("invalid action ");
+    }
+    return state;
+};
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, initState);
+
+    const { job, jobs } = state;
+
+    const inputRef = useRef();
+
+    const handleSumbit = () => {
+        dispatch(addJob(job));
+        dispatch(setJob(""));
+        inputRef.current.focus();
+    };
 
     return (
         <div className="App" style={{ padding: 50 }}>
+            <h3>Todo</h3>
             <input
-                value={name}
-                placeholder="Enter Name"
-                onChange={(e) => setName(e.target.value)}
+                ref={inputRef}
+                value={job}
+                placeholder="Enter to do..."
+                onChange={(e) => {
+                    dispatch(setJob(e.target.value));
+                }}
             />
-            <br />
-            <input
-                value={price}
-                placeholder="Enter Price"
-                onChange={(e) => setPrice(e.target.value)}
-            />
-            <br />
-            <button onClick={handleClick}>Add</button>
-            Total: {total}
+            <button onClick={handleSumbit}>Add</button>
             <ul>
-                {products.map((product, index) => (
+                {jobs.map((job, index) => (
                     <li key={index}>
-                        {product.name}-{product.price}
+                        {job}
+                        <span
+                            style={{ cursor: "pointer" }}
+                            onClick={() => dispatch(deleteJob(index))}
+                        >
+                            &times;
+                        </span>
                     </li>
                 ))}
             </ul>
